0e1d5b7fb4017bc0f6e81ad529160b38f2761155
Maxgis <maxinvc@126.com>
2013-10-23 21:42:14 +0800

    add geogetry type:line
    添加line类型，下次添加区类型

--- Symbol/symbol.js
+++ Symbol/symbol.js
@@ -1,6 +1,8 @@
 max.Symbol=function(option){
     this.fillStyle=option.fillStyle?option.fillStyle:'rgba(0,0,0,1)';
     this.fillSize=option.fillSize?option.fillSize:8;
+    this.lineWidth=option.lineWidth?option.lineWidth:4;
+    this.lineStyle=option.lineStyle?option.lineStyle:'rgba(0,0,0,1)';
 }
 
 max.Symbol.SimpleMarkerSymbol=function(option){
